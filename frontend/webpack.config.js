9fdabb0990f2bca0c6e47326771bf2af36854884
Pedro Roque <pedro.roque@outlook.com>
2021-12-28 18:02:18 -0300

    initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "app"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    historyApiFallback: true,
+    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({template: "./index.html"}),
+  ],
+};
